83c26c0531d67f727a60a00980c88a43fe659d5b
kdkdhoho <hkim4410@naver.com>
2025-10-03 18:44:13 +0900

    fix: br.close()가 먼저 호출되는 문제 해결

--- src/main/java/tobyspring/hellospring/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/WebApiExRateProvider.java
@@ -16,9 +16,9 @@ public class WebApiExRateProvider implements ExRateProvider {
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // HttpURLConnection 타입으로 받으면 다루기가 한결 수월해진다.
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream())); // InputStream: 네트워크로 넘어오는 데이터를 바이트 형태로 리턴해준다.
+        String response = br.lines().collect(Collectors.joining());
         br.close();
 
-        String response = br.lines().collect(Collectors.joining());
         ObjectMapper mapper = new ObjectMapper();
         ExchangeRateData data = mapper.readValue(response, ExchangeRateData.class);
         return data.rates().get("KRW");
