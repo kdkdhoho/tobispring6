d1d905b6614f05ba06168fbdde0098f720f361f6
kdkdhoho <hkim4410@naver.com>
2025-10-03 16:50:15 +0900

    comment: 메서드 추출의 장단점 작성

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -21,6 +21,9 @@ public class PaymentService {
     }
 
     private BigDecimal getExchangeRateKRW(String currency) throws IOException {
+    // 메서드로 분리하는 것으로도 충분히 코드가 보기 편안해졌다.
+    // 그럼에도 PaymentService 클래스 관점에서 보면 여전히 두 개의 서로 다른 관심사를 가지고 있다.
+    // 비즈니스 로직이 변경되거나 기술적 구현 방식이 변경되면 PaymentService는 변경되어야 한다.
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // HttpURLConnection 타입으로 받으면 다루기가 한결 수월해진다.
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream())); // InputStream: 네트워크로 넘어오는 데이터를 바이트 형태로 리턴해준다.
