33fe4bc4496007335efb874f8528b46bb3e63fab
kdkdhoho <hkim4410@naver.com>
2025-10-05 16:39:02 +0900

    refactor: 환율 API 응답 객체 이름 변경

--- src/main/java/tobyspring/hellospring/api/ErApiExRateExtractor.java
+++ src/main/java/tobyspring/hellospring/api/ErApiExRateExtractor.java
@@ -3,7 +3,7 @@ package tobyspring.hellospring.api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
-import tobyspring.hellospring.exrate.ExchangeRateData;
+import tobyspring.hellospring.exrate.ExRateData;
 
 public class ErApiExRateExtractor implements ExRateExtractor {
 
@@ -11,6 +11,6 @@ public class ErApiExRateExtractor implements ExRateExtractor {
 
     @Override
     public BigDecimal extract(String json) throws JsonProcessingException {
-        return mapper.readValue(json, ExchangeRateData.class).rates().get("KRW");
+        return mapper.readValue(json, ExRateData.class).rates().get("KRW");
     }
 }
--- src/main/java/tobyspring/hellospring/exrate/ExRateData.java
+++ src/main/java/tobyspring/hellospring/exrate/ExRateData.java
@@ -7,7 +7,7 @@ import java.util.Map;
 // ObjectMapper를 통해 Json의 값을 매핑할 때, Json에는 있지만 Record 타입에는 해당 변수를 받아줄 필드가 없으면 에러가 발생한다.
 // 하지만 모든 필드를 받기 싫은 경우, 아래 어노테이션을 달아주면 받아줄 필드가 없다면 해당 필드는 무시한다.
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ExchangeRateData(
+public record ExRateData(
         String result,
         Map<String, BigDecimal> rates
 ) {
