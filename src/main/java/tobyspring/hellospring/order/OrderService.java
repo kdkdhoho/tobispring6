3bb1db0f8cffa5265a5a992e9dfa448ae01d43b4
kdkdhoho <hkim4410@naver.com>
2025-10-06 15:21:24 +0900

    refactor: OrderService가 JPA 기술에 종속적인 JpaTransactionManager 대신, 스프링이 TransactionManager를 추상화한 PlatformTransactionManager를 의존하도록 수정

--- src/main/java/tobyspring/hellospring/order/OrderService.java
+++ src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -1,21 +1,17 @@
 package tobyspring.hellospring.order;
 
 import java.math.BigDecimal;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
-// 애플리케이션 서비스 객체는 외부 기술에 의존하면 안된다.
-// 여기서 외부 기술은 Jpa가 해당한다.
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final PlatformTransactionManager txManager;
 
-    // 문제점 2. JpaTransactionManager에 의존한다.
-    private final JpaTransactionManager txManager;
-
-    public OrderService(OrderRepository jpaOrderRepository, JpaTransactionManager txManager) {
+    public OrderService(OrderRepository jpaOrderRepository, PlatformTransactionManager txManager) {
         this.orderRepository = jpaOrderRepository;
         this.txManager = txManager;
     }
---
+++ src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
@@ -0,0 +1,28 @@
+package tobyspring.hellospring.order;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tobyspring.hellospring.OrderConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Import(OrderConfig.class)
+@ExtendWith(SpringExtension.class)
+public class OrderServiceSpringTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Test
+    void createOrderTest() {
+        // when
+        Order result = orderService.createOrder("0100", BigDecimal.TEN);
+
+        // then
+        assertThat(result.id()).isGreaterThan(1);
+    }
+}
