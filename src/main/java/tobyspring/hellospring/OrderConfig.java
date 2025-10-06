7e26e2c0c02fb334c757c494751e8047e7095bf8
kdkdhoho <hkim4410@naver.com>
2025-10-06 17:24:03 +0900

    refactor: `@Transactional` 어노테이션을 추가

--- src/main/java/tobyspring/hellospring/OrderConfig.java
+++ src/main/java/tobyspring/hellospring/OrderConfig.java
@@ -4,20 +4,18 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.PlatformTransactionManager;
 import tobyspring.hellospring.data.JdbcClientOrderRepository;
 import tobyspring.hellospring.order.OrderRepository;
 import tobyspring.hellospring.order.OrderService;
 import tobyspring.hellospring.order.OrderServiceImpl;
-import tobyspring.hellospring.order.OrderServiceTxProxy;
 
 @Configuration
 @Import(DataConfig.class)
 public class OrderConfig {
 
     @Bean
-    public OrderService orderService(OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
-        return new OrderServiceTxProxy(new OrderServiceImpl(orderRepository), transactionManager);
+    public OrderService orderService(OrderRepository orderRepository) {
+        return new OrderServiceImpl(orderRepository);
     }
 
     @Bean
--- src/main/java/tobyspring/hellospring/order/OrderServiceImpl.java
+++ src/main/java/tobyspring/hellospring/order/OrderServiceImpl.java
@@ -4,8 +4,10 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional // @Transactional 어노테이션을 붙임으로써, 스프링은 런타임에 해당 클래스에 트랜잭션 기능을 제공하는 프록시 오브젝트를 추가한다.
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
